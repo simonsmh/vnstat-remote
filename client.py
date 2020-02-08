c4d74f05347a383864c218bc3e6209f2d6a2adcf
simonsmh <simonsmh@gmail.com>
2020-02-08 21:45:31 +0800

    Force d mode to get more data

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import base64
 import json
 import logging
 import sys
+from datetime import datetime, timedelta
 from hashlib import blake2s
 
 from cryptography.fernet import Fernet
@@ -14,19 +15,28 @@ logging.basicConfig(
 logger = logging.getLogger("VnStat Client")
 
 
-async def get_vnstat(mode, addr):
-    host, port = addr
+async def get_vnstat(loop, addr):
     try:
-        reader, writer = await asyncio.open_connection(host, port, loop=loop)
+        reader, writer = await asyncio.open_connection(
+            addr.get("host"), addr.get("port"), loop=loop
+        )
     except ConnectionRefusedError:
         return False
-    writer.write(mode.encode())
+    begin = addr.get("begin")
+    if begin > datetime.now().day:
+        cycle = datetime.today().replace(day=begin) - timedelta(days=31)
+    else:
+        cycle = datetime.today().replace(day=begin)
+    cycle_date = cycle.strftime("%Y-%m-%d")
+    request_encrypted = f.encrypt(cycle_date.encode())
+    writer.write(request_encrypted)
     await writer.drain()
     data = await reader.read(4096)
     data_decrypted = f.decrypt(data)
     writer.close()
     data_decrypted = json.loads(data_decrypted)
-    if data_decrypted.get("result"):
+    if data_decrypted.get("error"):
+        logger.warning(f"{data_decrypted.get('error')}")
         return False
     return data_decrypted
 
@@ -37,14 +47,26 @@ def init_key(password):
     )
     return Fernet(key)
 
+def get_expect(dict):
+    return sum(dict)/len(dict)*31
 
 if __name__ == "__main__":
     password = sys.argv[1] if len(sys.argv) >= 2 else "test"
     f = init_key(password)
-    addrs = [("127.0.0.1", 10000), ("127.0.0.1", 10000)]
+    addrs = [
+        {"host": "127.0.0.1", "port": 10000, "begin": 9},
+        {"host": "127.0.0.1", "port": 10000, "begin": 7},
+    ]
     loop = asyncio.get_event_loop()
-    tasks = [get_vnstat("m", addr) for addr in addrs]
-    result = loop.run_until_complete(asyncio.gather(*tasks))
-    for i, r in enumerate(result):
-        if r:
-            logger.info(f"Received from {addrs[i][0]}:\n{r}")
+    tasks = [get_vnstat(loop, addr) for addr in addrs]
+    results = loop.run_until_complete(asyncio.gather(*tasks))
+    for i, result in enumerate(results):
+        if not result:
+            continue
+        logger.info(f'Received from {addrs[i].get("host")}')
+        interface = result.get("interfaces")[0]
+        logger.info(f'Parsing Interface {interface.get("name")}')
+        rx = [day.get("rx") for day in interface.get("traffic").get("day")]
+        tx = [day.get("tx") for day in interface.get("traffic").get("day")]
+        logger.info(f"Rx: {sum(rx)}, Tx: {sum(tx)}")
+        logger.info(f"Expect Rx: {get_expect(rx)}, Expect Tx: {get_expect(tx)}")
--- server.py
+++ server.py
@@ -15,26 +15,26 @@ logger = logging.getLogger("VnStat Server")
 
 
 async def send_vnstat(reader, writer):
-    data = await reader.read(1)
-    addr = writer.get_extra_info("peername")
-    logger.info(f"Received {data} from {addr}")
-
-    mode = data
+    data = await reader.read(100)
+    data_decrypted = f.decrypt(data)
+    logger.info(f"Received {data_decrypted} from {writer.get_extra_info('peername')}")
+    begin = data_decrypted.decode()
     try:
         sp = subprocess.run(
-            ["vnstat", "--json", mode, "--limit", "1"], capture_output=True, check=True
+            ["vnstat", "-b", begin, "--json", "d", "--limit", "31"],
+            capture_output=True,
+            check=True,
         )
         result = sp.stdout
     except subprocess.CalledProcessError as err:
         logger.warning(f"Subprocess: {err}")
-        result = '{"result": "error"}'.encode()
-
+        result = f'{"error": "{err}"}'.encode()
     result_encrypted = f.encrypt(result)
     logger.debug(f"Sending:\n{result}\n{result_encrypted}")
     writer.write(result_encrypted)
     await writer.drain()
     writer.close()
-    
+
 
 def init_key(password):
     key = base64.urlsafe_b64encode(
@@ -47,10 +47,8 @@ if __name__ == "__main__":
     password = sys.argv[1] if len(sys.argv) >= 2 else "test"
     port = int(sys.argv[2]) if len(sys.argv) >= 3 else 10000
     f = init_key(password)
-    
     loop = asyncio.get_event_loop()
     coro = asyncio.start_server(send_vnstat, port=port, reuse_port=True, loop=loop)
     server = loop.run_until_complete(coro)
-    addr = server.sockets[0].getsockname()
-    logger.info(f"Serving on {addr}")
+    logger.info(f"Serving on {server.sockets[0].getsockname()}")
     loop.run_forever()
